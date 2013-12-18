7d5fcad77d66fdd8439c4986462534f9a5b94779
Steven Carter <stevenc@voxtelecom.co.za>
2013-12-18 14:11:35 +0200

    * Added the configuration page * Split the router file out of the main.go * Added the CSS for the configuration page

--- src/controllers/configuration.go
+++ src/controllers/configuration.go
@@ -12,8 +12,8 @@ func handleConfiguration(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Configuration: Handling '%v' Request to: '%v", r.Method, html.EscapeString(r.URL.Path))
 	// Check what type of request was made (GET / POST)
 	if r.Method == "GET" {
-	  t, _ := template.ParseFiles("tmpl/index.html")
-	  t.Execute(w, t)
+		t, _ := template.ParseFiles("tmpl/index.html")
+		t.Execute(w, t)
 	}
 }
 
