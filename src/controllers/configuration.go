5d3f382bb132844ea9409d53a28ae347484724c7
Steven Carter <stevenc@voxtelecom.co.za>
2013-12-23 11:24:08 +0200

    * Added a sample template sb-admin * Added in the create / update handlers for the index page * Added in the bootstrap libraries

--- src/controllers/configuration.go
+++ src/controllers/configuration.go
@@ -13,20 +13,41 @@ type Page struct {
 }
 
 // Handles the Requests to the /config resource
-func handleConfiguration(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Configuration: Handling '%v' Request to: '%v", r.Method, html.EscapeString(r.URL.Path))
 	// Check what type of request was made (GET / POST)
-	if r.Method == "GET" {
-		routes := models.Load()
-		page := &Page{Routes: routes}
-		t, _ := template.ParseFiles("tmpl/index.html")
-		t.Execute(w, page)
-	}
+	routes := models.Load()
+	page := &Page{Routes: routes}
+	t := template.Must(template.ParseGlob("tmpl/*.html"))
+	// t, _ := template.ParseGlob("tmpl/*")
+	t.ExecuteTemplate(w, "indexPage", page)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Configuration: Handling '%v' Request to: '%v", r.Method, html.EscapeString(r.URL.Path))
+	// Check what type of request was made (GET / POST)
+	routes := models.Load()
+	page := &Page{Routes: routes}
+	t, _ := template.ParseFiles("tmpl/index.html")
+	t.Execute(w, page)
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Configuration: Handling '%v' Request to: '%v", r.Method, html.EscapeString(r.URL.Path))
+	// Check what type of request was made (GET / POST)
+	routes := models.Load()
+	page := &Page{Routes: routes}
+	t, _ := template.ParseFiles("tmpl/index.html")
+	t.Execute(w, page)
 }
 
 // Loads the Configuration Controller for Gouter
 func Load() {
 	log.Printf("Configuration: Loading the configuration handlers.")
-	http.HandleFunc("/config", handleConfiguration)
+	http.HandleFunc("/config", index)
+	http.HandleFunc("/config/new", create)
+	http.HandleFunc("/config/edit", update)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./tmpl/css"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./tmpl/js"))))
+	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("./tmpl/fonts"))))
 }
